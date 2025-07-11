aae4e6d37370c351a2f2d270750e4dd4475e8417
Mariam Samaké <samakem964@gmail.com>
2025-07-11 19:51:41 +0000

    fichier exo 1 et 2

---
+++ exercice2.py
@@ -0,0 +1,9 @@
+
+def nombre_voisinages_ospf(nb_routeurs):
+    if nb_routeurs <= 1:
+        return 0
+    return nb_routeurs - 1
+
+nb_routeurs = int(input("Saisissez le nombre  de routeurs dans la zone : "))
+print("Le nombre de voisinages attendus est :", nombre_voisinages_ospf(nb_routeurs))
+
